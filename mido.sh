8d59178de63f2065ba64b1294ca7b8918e84f15a
malkist01 <164641045+malkist01@users.noreply.github.com>
2026-03-23 03:50:32 +0700

    Update mido.sh

--- mido.sh
+++ mido.sh
@@ -23,7 +23,7 @@ LINUX_VER=$(make kernelversion 2>/dev/null)
 # KernelSU
 # ============================
 
-curl -LSs "https://raw.githubusercontent.com/malkist01/SU/main/kernel/setup.sh" | bash -s main
+curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/legacy_susfs/kernel/setup.sh" | bash -s legacy_susfs
 
 chmod +x test.patch
 chmod +x susfs_1.patch
