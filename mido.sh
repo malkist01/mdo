df462e0e28beb5ead5b48fa75f0dad07ce1d8248
malkist01 <164641045+malkist01@users.noreply.github.com>
2026-03-23 03:38:21 +0700

    Update mido.sh

--- mido.sh
+++ mido.sh
@@ -30,8 +30,6 @@ chmod +x susfs_1.patch
 chmod +x susfs_2.patch
 chmod +x susfs_3.patch
 patch -p1 < test.patch
-patch -p1 < susfs_1.patch
-patch -p1 < susfs_2.patch
 patch -p1 < susfs_3.patch
 
 # ============================
