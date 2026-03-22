bebc200013a5fb23a9eea8bd285f806a4e1475a1
malkist01 <164641045+malkist01@users.noreply.github.com>
2026-03-22 18:36:58 +0700

    Update mido.sh

--- mido.sh
+++ mido.sh
@@ -23,6 +23,8 @@ LINUX_VER=$(make kernelversion 2>/dev/null)
 # KernelSU
 # ============================
 chmod +x test.patch
+chmod +x patch1.patch
+patch -p1 < patch1.patch
 patch -p1 < test.patch
 
 echo "CONFIG_KSU=y" >> ./arch/arm64/configs/mido_defconfig
