a2cba2c495de89a207401b91e6e683f56d81d2d4
malkist01 <164641045+malkist01@users.noreply.github.com>
2026-03-23 23:36:44 +0700

    Update mido.sh

--- mido.sh
+++ mido.sh
@@ -27,7 +27,7 @@ ccache --set-config=compression=true
 # KernelSU
 # ============================
 
-curl -LSs "https://raw.githubusercontent.com/malkist01/KernelSU-Next/main/kernel/setup.sh" | bash -s main
+curl -LSs "https://raw.githubusercontent.com/Sorayukii/KernelSU-Next/susfs-main/kernel/setup.sh" | bash -s susfs-main
 
 # ============================
 # Variabel Telegram dan Device Info
