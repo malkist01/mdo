becd6d5b1e56042565a55c36ce44e3bc77cdf84c
malkist01 <164641045+malkist01@users.noreply.github.com>
2026-03-23 23:30:42 +0700

    Add files via upload

--- include/linux/susfs_def.h
+++ include/linux/susfs_def.h
@@ -2,6 +2,10 @@
 #define KSU_SUSFS_DEF_H
 
 #include <linux/bits.h>
+#include <linux/version.h> // We need check kernel version.
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 12, 0)
+#include <linux/cred.h>
+#endif
 
 /********/
 /* ENUM */
@@ -9,8 +13,8 @@
 /* shared with userspace ksu_susfs tool */
 #define SUSFS_MAGIC 0xFAFAFAFA
 #define CMD_SUSFS_ADD_SUS_PATH 0x55550
-#define CMD_SUSFS_SET_ANDROID_DATA_ROOT_PATH 0x55551
-#define CMD_SUSFS_SET_SDCARD_ROOT_PATH 0x55552
+#define CMD_SUSFS_SET_ANDROID_DATA_ROOT_PATH 0x55551 /* deprecated */
+#define CMD_SUSFS_SET_SDCARD_ROOT_PATH 0x55552 /* deprecated */
 #define CMD_SUSFS_ADD_SUS_PATH_LOOP 0x55553
 #define CMD_SUSFS_ADD_SUS_MOUNT 0x55560 /* deprecated */
 #define CMD_SUSFS_HIDE_SUS_MNTS_FOR_NON_SU_PROCS 0x55561
@@ -41,13 +45,12 @@
 #define TRY_UMOUNT_DEFAULT 0 /* used by susfs_try_umount() */
 #define TRY_UMOUNT_DETACH 1 /* used by susfs_try_umount() */
 
-#define DEFAULT_KSU_MNT_ID 500000 /* used by mount->mnt_id */
-#define DEFAULT_SUS_MNT_ID_FOR_KSU_PROC_UNSHARE 1000000 /* used by vfsmount->susfs_mnt_id_backup */
+#define VFSMOUNT_MNT_FLAGS_KSU_UNSHARED_MNT 0x80000000 /* used for mounts that are unshared by ksu process */
+#define DEFAULT_KSU_MNT_ID 500000 /* used for mounts created or single cloned by ksu process */
 #define DEFAULT_KSU_MNT_GROUP_ID 5000 /* used by mount->mnt_group_id */
 
 /*
- * mount->mnt.susfs_mnt_id_backup => storing original mount's mnt_id
- * inode->i_state => A 'unsigned long' type storing flag 'AS_FLAGS_', bit 1 to 31 is not usable since 6.12
+ * inode->i_mapping->flags => A 'unsigned long' type storing flag 'AS_FLAGS_', bit 1 to 31 is not usable since 6.12
  * nd->state => storing flag 'ND_STATE_'
  * nd->flags => storing flag 'ND_FLAGS_'
  * task_struct->thread_info.flags => storing flag 'TIF_'
@@ -59,23 +62,53 @@
 #define AS_FLAGS_SUS_MOUNT 34
 #define AS_FLAGS_SUS_KSTAT 35
 #define AS_FLAGS_OPEN_REDIRECT 36
-#define AS_FLAGS_ANDROID_DATA_ROOT_DIR 37
-#define AS_FLAGS_SDCARD_ROOT_DIR 38
 #define AS_FLAGS_SUS_MAP 39
-#define BIT_SUS_PATH BIT(33)
-#define BIT_SUS_MOUNT BIT(34)
-#define BIT_SUS_KSTAT BIT(35)
-#define BIT_OPEN_REDIRECT BIT(36)
-#define BIT_ANDROID_DATA_ROOT_DIR BIT(37)
-#define BIT_ANDROID_SDCARD_ROOT_DIR BIT(38)
-#define BIT_SUS_MAPS BIT(39)
 
 #define ND_STATE_LOOKUP_LAST 32
 #define ND_STATE_OPEN_LAST 64
-#define ND_STATE_LAST_SDCARD_SUS_PATH 128
 #define ND_FLAGS_LOOKUP_LAST		0x2000000
  
 #define MAGIC_MOUNT_WORKDIR "/debug_ramdisk/workdir"
+
+/* From KernelSU */
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 2, 0)
+typedef const struct qstr *susfs_fname_t;
+#define susfs_fname_len(f) ((f)->len)
+#define susfs_fname_arg(f) ((f)->name)
+#else
+typedef const unsigned char *susfs_fname_t;
+#define susfs_fname_len(f) (strlen(f))
+#define susfs_fname_arg(f) (f)
+#endif
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 9, 0)
+#define SUSFS_DECL_FSNOTIFY_OPS(name)                                            \
+int name(struct fsnotify_mark *mark, u32 mask, struct inode *inode,    \
+struct inode *dir, const struct qstr *file_name, u32 cookie)
+#elif LINUX_VERSION_CODE >= KERNEL_VERSION(5, 2, 0)
+#define SUSFS_DECL_FSNOTIFY_OPS(name)                                            \
+int name(struct fsnotify_group *group, struct inode *inode, u32 mask,  \
+const void *data, int data_type, susfs_fname_t file_name,       \
+u32 cookie, struct fsnotify_iter_info *iter_info)
+#elif LINUX_VERSION_CODE >= KERNEL_VERSION(4, 18, 0)
+#define SUSFS_DECL_FSNOTIFY_OPS(name)                                            \
+int name(struct fsnotify_group *group, struct inode *inode, u32 mask,  \
+const void *data, int data_type, susfs_fname_t file_name,       \
+u32 cookie, struct fsnotify_iter_info *iter_info)
+#elif LINUX_VERSION_CODE >= KERNEL_VERSION(4, 12, 0)
+#define SUSFS_DECL_FSNOTIFY_OPS(name)                                            \
+int name(struct fsnotify_group *group, struct inode *inode,            \
+struct fsnotify_mark *inode_mark,                             \
+struct fsnotify_mark *vfsmount_mark, u32 mask,                \
+const void *data, int data_type, susfs_fname_t file_name,       \
+u32 cookie, struct fsnotify_iter_info *iter_info)
+#else
+#define SUSFS_DECL_FSNOTIFY_OPS(name)                                            \
+int name(struct fsnotify_group *group, struct inode *inode,            \
+struct fsnotify_mark *inode_mark,                             \
+struct fsnotify_mark *vfsmount_mark, u32 mask, void *data,    \
+int data_type, susfs_fname_t file_name, u32 cookie)
+#endif
 
 static inline bool susfs_is_current_proc_umounted(void) {
 	return test_ti_thread_flag(&current->thread_info, TIF_PROC_UMOUNTED);
@@ -84,4 +117,27 @@ static inline bool susfs_is_current_proc_umounted(void) {
 static inline void susfs_set_current_proc_umounted(void) {
 	set_ti_thread_flag(&current->thread_info, TIF_PROC_UMOUNTED);
 }
+
+static inline bool susfs_is_current_proc_umounted_app(void) {
+	return (test_ti_thread_flag(&current->thread_info, TIF_PROC_UMOUNTED) &&
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 12, 0)
+	__kuid_val(current_uid()) >= 10000);
+#else
+	current_uid().val >= 10000);
+#endif
+}
+
+#define SUSFS_IS_INODE_SUS_MAP(inode) \
+inode && inode->i_mapping && \
+unlikely(test_bit(AS_FLAGS_SUS_MAP, &inode->i_mapping->flags)) && \
+susfs_is_current_proc_umounted_app()
+
+#define SUSFS_IS_INODE_OPEN_REDIRECT_WITHOUT_UID_CHECK(inode) \
+inode && inode->i_mapping && \
+unlikely(test_bit(AS_FLAGS_OPEN_REDIRECT, &inode->i_mapping->flags))
+
+#define SUSFS_IS_INODE_OPEN_REDIRECT(inode) \
+inode && inode->i_mapping && \
+unlikely(test_bit(AS_FLAGS_OPEN_REDIRECT, &inode->i_mapping->flags)) && \
+susfs_is_current_proc_umounted_app()
 #endif // #ifndef KSU_SUSFS_DEF_H
