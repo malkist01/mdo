e9ebe6ed4b3fd52716c4df5b9f214015218b50ee
malkist01 <164641045+malkist01@users.noreply.github.com>
2026-03-24 00:11:22 +0700

    Update mido.sh

--- mido.sh
+++ mido.sh
@@ -27,7 +27,9 @@ ccache --set-config=compression=true
 # KernelSU
 # ============================
 
-curl -LSs "https://raw.githubusercontent.com/Sorayukii/KernelSU-Next/susfs-1512/kernel/setup.sh" | bash -s susfs-1512
+curl -LSs "https://raw.githubusercontent.com/ReSukiSU/ReSukiSU/refs/heads/main/kernel/setup.sh" | bash -s main
+curl -LSs "https://raw.githubusercontent.com/JackA1ltman/NonGKI_Kernel_Build_2nd/refs/heads/mainline/Patches/syscall_hook_patches.sh" | bash -s
+curl -LSs "https://raw.githubusercontent.com/JackA1ltman/NonGKI_Kernel_Build_2nd/refs/heads/mainline/Patches/backport_patches.sh" | bash -s
 
 # ============================
 # Variabel Telegram dan Device Info
