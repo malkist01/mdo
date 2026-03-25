71e7ead542f3c9dce8d5f3a3bea8165282097a47
malkist01 <164641045+malkist01@users.noreply.github.com>
2026-03-25 18:10:50 +0700

    Update mido.sh

--- mido.sh
+++ mido.sh
@@ -23,6 +23,8 @@ export KBUILD_BUILD_HOST="phone"
 # ============================
 
 curl -LSs "https://raw.githubusercontent.com/ReSukiSU/ReSukiSU/refs/heads/main/kernel/setup.sh" | bash -s main
+curl -LSs "https://raw.githubusercontent.com/JackA1ltman/NonGKI_Kernel_Build_2nd/refs/heads/mainline/Patches/syscall_hook_patches.sh" | bash -s
+            curl -LSs "https://raw.githubusercontent.com/JackA1ltman/NonGKI_Kernel_Build_2nd/refs/heads/mainline/Patches/backport_patches.sh" | bash -s
 curl -LSs "https://raw.githubusercontent.com/JackA1ltman/NonGKI_Kernel_Build_2nd/refs/heads/mainline/Patches/susfs_inline_hook_patches.sh" | bash -s
 
 # ============================
